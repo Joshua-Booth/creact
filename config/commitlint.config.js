68fe594cc548b9890fea916a6e73ce12f87d62c8
Joshua Booth <me@joshuabooth.nz>
2021-06-13 22:14:36 +1200

    ci: allow longer line lengths for commits

--- config/commitlint.config.js
+++ config/commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+    "body-max-line-length": [2, "always", 160],
+    "footer-max-line-length": [0, "always", 160],
+  },
+};
