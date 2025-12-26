a5f07c56fa6a83083f03592b5f2ccdd153f28602
Joshua Booth <me@joshuabooth.nz>
2025-12-26 15:41:08 +1300

    chore: remove all old JavaScript files and SCSS directories

---
+++ src/components/Search/CornerSearch/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CornerSearch() {
+  return (
+    <div className="corner-search">
+      Search component (Algolia integration coming soon)
+    </div>
+  )
+}
